47a1f0d6cab373acd42c1675097bbc536b1a6308
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-08-26 20:36:01 +0800

    kernel: ksud: migrate ksud execution to security_bprm_check (tiann#2653)
    This migrates ksud execution decision-making to bprm_check_security.
    This requires passing proper argv and envp to a modified _ksud handler
    aptly named 'ksu_handle_bprm_ksud'.
    
    Introduces:
    int ksu_handle_bprm_ksud(const char *filename, const char *argv1,
    const char *envp, size_t envp_len)
    
    which is adapted from:
    int ksu_handle_execveat_ksud(int *fd, struct filename **filename_ptr,
    struct user_arg_ptr *argv,
    struct user_arg_ptr *envp,
    int *flags)
    
    ksu_handle_bprm_ksud handles all the decision making, it decides when it is
    time to apply_kernelsu_rules depending if it sees "second_stage".
    
    For LSM hook, turns out we can pull out argv and envp from mm_struct.
    The code in here explains itself on how to do it.
    
    whole blob exists on arg_start to arg_end, so we just pull it out and grab next
    array after the first null terminator.
    
    as for envp, we pass the pointer then hunt for it when needed
    
    My reasoning on adding a fallback on usercopy is that on some devices a fault
    happens, and it copies garbled data. On my creation of this, I actually had to lock
    that _nofault copy on a spinlock as a way to mimic preempt_disable/enable without
    actually doing it. As per user reports, no failed _nofault copies anyway but we
    have-to-have a fallback for resilience.
    
    References:
    - old version1 https://github.com/backslashxx/KernelSU/commit/6efcd8193e62d13a4e62cda0ce54d6770260c680
    - old version2 https://github.com/backslashxx/KernelSU/commit/37d5938e66db2578acbd763072533245e50742e1
    - bad usercopy #21
    
    This now provides a small helper function, ksu_copy_from_user_retry, which explains
    itself. First we attempt a _nofault copy, if that fails, we try plain.
    
    With that, It also provides an inlined copy_from_user_nofault for < 5.8.
    
    While using strncpy_from_user_nofault was considered, this wont do, this will
    only copy up to the first \0.
    
    devlog:
    https://github.com/ximi-libra-test/android_kernel_xiaomi_libra/compare/16e5dce9e7e...16c1f5f521a
    https://github.com/backslashxx/mojito_krenol/compare/28642e60d7b...728de0c571c
    
    References:
    https://elixir.bootlin.com/linux/v4.14.1/source/include/linux/mm_types.h#L429
    https://elixir.bootlin.com/linux/v4.14.1/source/include/linux/lsm_hooks.h
    
    Stale: https://github.com/tiann/KernelSU/pull/2653
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/ksud.h
+++ kernel/ksud.h
@@ -16,6 +16,11 @@ bool ksu_is_safe_mode(void);
 
 int nuke_ext4_sysfs(const char* mnt);
 
+extern bool ksu_execveat_hook __read_mostly;
+extern int ksu_handle_pre_ksud(const char *filename);
+extern int ksu_handle_bprm_ksud(const char *filename, const char *argv1,
+				const char *envp, size_t envp_len);
+
 extern u32 ksu_file_sid;
 extern bool ksu_module_mounted;
 extern bool ksu_boot_completed;
