c0daf62ff6db32576a2f54a470bd3594481c18ff
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-11-18 11:13:26 +0800

    kernel: remove unsupportable code
    Since upstream has way different methods, via kprobes/kretprobes/LKM/tracepoints,
    cleaning up support for unsupportable code is kind of a must. This simplifies porting
    downstream changes, restoring old components, debloat, and makes it easier to maintain
    downstream, e.g. avoiding excessive use of conditionals (ifdef hell).
    
    Theres now massive code divergence so this has to be done.
    We will restore compatibility to upstream with the next commits.
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -2,70 +2,54 @@
 #include <linux/fs.h>
 #include <linux/kobject.h>
 #include <linux/module.h>
-#include <linux/workqueue.h>
 
 #include "allowlist.h"
+#include "core_hook.h"
 #include "feature.h"
 #include "klog.h" // IWYU pragma: keep
+#include "ksu.h"
 #include "throne_tracker.h"
-#include "syscall_hook_manager.h"
+#include "sucompat.h"
 #include "ksud.h"
 #include "supercalls.h"
+
+extern void ksu_supercalls_init();
 
 int __init kernelsu_init(void)
 {
 #ifdef CONFIG_KSU_DEBUG
-    pr_alert("*************************************************************");
-    pr_alert("**     NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE    **");
-    pr_alert("**                                                         **");
-    pr_alert("**         You are running KernelSU in DEBUG mode          **");
-    pr_alert("**                                                         **");
-    pr_alert("**     NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE    **");
-    pr_alert("*************************************************************");
+	pr_alert("*************************************************************");
+	pr_alert("**     NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE    **");
+	pr_alert("**                                                         **");
+	pr_alert("**         You are running KernelSU in DEBUG mode          **");
+	pr_alert("**                                                         **");
+	pr_alert("**     NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE    **");
+	pr_alert("*************************************************************");
 #endif
 
-    ksu_feature_init();
+	ksu_feature_init();
 
-    ksu_supercalls_init();
+	ksu_supercalls_init();
 
-    ksu_syscall_hook_manager_init();
+	ksu_core_init();
 
-    ksu_allowlist_init();
+	ksu_allowlist_init();
 
-    ksu_throne_tracker_init();
+	ksu_throne_tracker_init();
 
-#ifdef CONFIG_KPROBES
-    ksu_ksud_init();
-#else
-    pr_alert("KPROBES is disabled, KernelSU may not work, please check https://kernelsu.org/guide/how-to-integrate-for-non-gki.html");
-#endif
-
-#ifdef MODULE
-#ifndef CONFIG_KSU_DEBUG
-    kobject_del(&THIS_MODULE->mkobj.kobj);
-#endif
-#endif
-    return 0;
+	return 0;
 }
 
 extern void ksu_observer_exit(void);
 void kernelsu_exit(void)
 {
-    ksu_allowlist_exit();
+	ksu_allowlist_exit();
 
-    ksu_throne_tracker_exit();
+	ksu_throne_tracker_exit();
 
-    ksu_observer_exit();
+	ksu_observer_exit();
 
-#ifdef CONFIG_KPROBES
-    ksu_ksud_exit();
-#endif
-
-    ksu_syscall_hook_manager_exit();
-
-    ksu_supercalls_exit();
-
-    ksu_feature_exit();
+	ksu_feature_exit();
 }
 
 module_init(kernelsu_init);
